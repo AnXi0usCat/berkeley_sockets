6823bc43bea09fb11dd36e923293141daacbc68d
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2025-05-03 11:03:02 +0100

    clone impl

--- src/stream.rs
+++ src/stream.rs
@@ -1,4 +1,6 @@
-use crate::reactor::{CmdTx, EventRx};
+use std::sync::Arc;
+
+use crate::reactor::{CmdTx, Event, EventRx};
 use crate::{
     reactor::{Cmd, Reactor},
     socket::Socket,
@@ -9,7 +11,7 @@ pub struct AsyncTcpListener {
     socket: Socket,
     reactor: Reactor,
     cmd_tx: CmdTx,
-    event_rx: EventRx,
+    event_rx: Arc<EventRx>,
 }
 
 impl AsyncTcpListener {
@@ -29,8 +31,35 @@ impl AsyncTcpListener {
             socket: sc,
             reactor,
             cmd_tx: tx,
-            event_rx: rx,
+            event_rx: Arc::new(rx),
         })
+    }
+
+    pub async fn accept(&self) -> Result<AsyncTcpStream, String> {
+        while let Ok(event) = self.event_rx.try_recv() {
+            if let Event::NewConnection(_fd) = event {
+                if let Ok(client) = self.socket.accept_nonblocking() {
+                    match client {
+                        Some(socket) => {
+                            self.cmd_tx
+                                .send(Cmd::Add(socket.fd, true, false))
+                                .map_err(|e| {
+                                    format!("Failed to send client fd to the reactor: {e}")
+                                })?;
+                            return Ok(AsyncTcpStream::new(
+                                socket,
+                                self.cmd_tx.clone(),
+                                Arc::clone(&self.event_rx),
+                            ));
+                        }
+                        None => {
+                            continue;
+                        }
+                    }
+                };
+            };
+        }
+        Err("Stream channle is closed".into())
     }
 }
 
@@ -38,5 +67,15 @@ impl AsyncTcpListener {
 pub struct AsyncTcpStream {
     socket: Socket,
     cmd_tx: CmdTx,
-    event_rx: EventRx,
+    event_rx: Arc<EventRx>,
+}
+
+impl AsyncTcpStream {
+    pub fn new(socket: Socket, cmd_tx: CmdTx, event_rx: Arc<EventRx>) -> Self {
+        AsyncTcpStream {
+            socket,
+            cmd_tx,
+            event_rx,
+        }
+    }
 }
